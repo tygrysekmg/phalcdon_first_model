e6c93e8d4e00130006805ed07cb5ac2861aec9a9
root <root@grzegorz-php72.com>
2018-03-14 15:00:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# phalcon_first_own_controller
+follow for images p4.png and p5.png
---
+++ app/models/Users.php
@@ -0,0 +1,77 @@
+<?php
+
+class Users extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     * @Primary
+     * @Identity
+     * @Column(column="id", type="integer", length=11, nullable=false)
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     * @Column(column="name", type="string", length=25, nullable=false)
+     */
+    public $name;
+
+    /**
+     *
+     * @var string
+     * @Column(column="emailid", type="string", length=50, nullable=false)
+     */
+    public $emailid;
+
+    /**
+     *
+     * @var integer
+     * @Column(column="contactNumber", type="integer", length=11, nullable=false)
+     */
+    public $contactNumber;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSchema("users");
+        $this->setSource("users");
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'users';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users[]|Users|\Phalcon\Mvc\Model\ResultSetInterface
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users|\Phalcon\Mvc\Model\ResultInterface
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+}
